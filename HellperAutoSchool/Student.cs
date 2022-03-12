4f8395ec8a39ce333999c39893c4b77679504e98
k1rm4n <lal-max@mail.ru>
2022-03-12 21:03:17 +0300

    .

--- HellperAutoSchool/Student.cs
+++ HellperAutoSchool/Student.cs
@@ -5,36 +5,87 @@ using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Data;
+using System.IO;
 
 namespace HellperAutoSchool
 {
     class Student : DateBaseConnect
     {
-        static int x = 860;
-
         DataGridView tableStudents = new DataGridView()
         {
             Location = new Point(30, 30),
-            Width = 800,
+            Width = 750,
             Height = 400,
-            BackgroundColor = Color.GreenYellow,
             AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells,
             Font = new Font("Arial", 12)
         };
 
+        GroupBox groupBox = new GroupBox()
+        {
+            Location = new Point(785, 100),
+            Width = 192,
+            Text = "Инструменты"
+        };
+
         Button btnAdd = new Button()
         {
-            Location = new Point(x, 100),
-            Text = "Add"
+            Location = new Point(10, 20),
+            Width = 25, Height = 25,
+            BackgroundImage = Image.FromFile("C:/Users/lal-m/source/repos/HellperAutoSchool/HellperAutoSchool/Photo/add.png"),
+            BackgroundImageLayout = ImageLayout.Stretch,
+            FlatStyle = FlatStyle.Flat
         };
+
+        Button btnDelete = new Button()
+        {
+            Location = new Point(38, 20),
+            Width = 25,
+            Height = 25,
+            BackgroundImage = Image.FromFile("C:/Users/lal-m/source/repos/HellperAutoSchool/HellperAutoSchool/Photo/delete.png"),
+            BackgroundImageLayout = ImageLayout.Stretch,
+            FlatStyle = FlatStyle.Flat
+        };
+
+        Button btnEdit = new Button()
+        {
+            Location = new Point(65, 20),
+            Width = 25,
+            Height = 25,
+            BackgroundImage = Image.FromFile("C:/Users/lal-m/source/repos/HellperAutoSchool/HellperAutoSchool/Photo/edit.png"),
+            BackgroundImageLayout = ImageLayout.Stretch,
+            FlatStyle = FlatStyle.Flat
+        };
+
+        Button btnUpdate = new Button()
+        {
+            Location = new Point(90, 20),
+            Width = 25,
+            Height = 25,
+            BackgroundImage = Image.FromFile("C:/Users/lal-m/source/repos/HellperAutoSchool/HellperAutoSchool/Photo/update.png"),
+            BackgroundImageLayout = ImageLayout.Stretch,
+            FlatStyle = FlatStyle.Flat
+        };
+
+        public void AddButtonOnGroupBox()
+        {
+            btnAdd.FlatAppearance.BorderSize = 0;
+            btnDelete.FlatAppearance.BorderSize = 0;
+            btnEdit.FlatAppearance.BorderSize = 0;
+            btnUpdate.FlatAppearance.BorderSize = 0;
+            groupBox.Controls.Add(btnAdd); 
+            groupBox.Controls.Add(btnDelete);
+            groupBox.Controls.Add(btnEdit);
+            groupBox.Controls.Add(btnUpdate);
+
+            btnAdd.Click += BtnAdd_Click;
+        }
 
         public void AddTableStudents(Form form)
         {
             form.Controls.Add(tableStudents);
             LoadDataStudents();
-            form.Controls.Add(btnAdd);
-
-            btnAdd.Click += BtnAdd_Click;
+            AddButtonOnGroupBox();
+            form.Controls.Add(groupBox);
         }
 
         private void BtnAdd_Click(object sender, EventArgs e)
