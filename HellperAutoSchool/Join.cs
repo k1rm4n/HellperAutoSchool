7de572357aa1288bb1027ad1fb27fc175d2fccdc
k1rm4n <81237385+k1rm4n@users.noreply.github.com>
2022-03-16 09:42:27 +0300

    16.03.2022

---
+++ HellperAutoSchool/Join.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Data;
+using System.Collections;
+
+namespace HellperAutoSchool
+{
+    class Join : DataBaseConnect
+    {
+        DataGridView tableGroups = new DataGridView()
+        {
+            Location = new Point(15, 50),
+            Width = 750,
+            Height = 400,
+            AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells,
+            Font = new Font("Arial", 12)
+        };
+
+        public void AddTableJoins()
+        {
+
+        }
+    }
+}
